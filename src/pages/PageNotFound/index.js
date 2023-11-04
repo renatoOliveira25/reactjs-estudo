260d3ee2c631edc6cd1714f7f8157e26d514e921
Renato Oliveira <renato.loliveira89@gmail.com>
2023-11-04 13:37:38 -0300

    aula 09 - criar página 404 e ajustes no css

---
+++ src/pages/PageNotFound/index.js
@@ -0,0 +1,19 @@
+import error404 from "./erro404.png"
+import styles from "./PageNotFound.module.css"
+import Header from "../../components/Header"
+import Footer from "../../components/Footer"
+
+function PageNotFound() {
+    return (
+        <>
+            <Header />
+            <section className={styles.container}>
+                <h2>Ops! Conteúdo não localizado.</h2>
+                <img src={error404} alt="Logo da página não localizada" />
+            </section>
+            <Footer />
+        </>
+    );
+}
+
+export default PageNotFound
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home"
 import Assistir from "./pages/Assistir"
+import PageNotFound from "./pages/PageNotFound";
 
 function AppRoutes() {
     return (
@@ -8,6 +9,7 @@ function AppRoutes() {
             <Routes>
                 <Route path="/" element={ <Home /> }></Route>
                 <Route path="/assistir" element={ <Assistir /> }></Route>
+                <Route path="*" element={ <PageNotFound /> }></Route>
             </Routes>
         </BrowserRouter>
     );
