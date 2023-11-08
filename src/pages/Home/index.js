10b6acb7ca554900b6b5a49e0d9b6499551d90a9
Renato Oliveira <renato.loliveira89@gmail.com>
2023-11-07 21:20:50 -0300

    aula 11 - react hooks e useState
    Aplicando conceito de Hooks e useState

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Banner from "../../components/Banner";
 import Card from "../../components/Card";
 import Category, { categorias, filtrarCategoria } from "../../components/Category";
@@ -6,19 +7,37 @@ import Footer from "../../components/Footer";
 import Header from "../../components/Header";   
 
 function Home() {
+
+  let nomeDigitado = ''
+
+  function pegarNome(event) {
+    console.log(event.target.value);
+    //nomeDigitado = event.target.value;
+    setNome(event.target.value);
+  }
+
+  // Hook (gancho) -> useState
+  const [nome, setNome] = useState('');
+
   return (
     <>
       <Header />
       <Banner imagem="home" />
       <Container>
 
-        {
+        <input 
+          type="text"
+          placeholder="Nome"
+          onChange={ pegarNome }
+        />
+        <h2>{ nome }</h2>
+        {/* {
           categorias.map((categoria, indice) => 
             <Category categoria={ categoria }>
               {filtrarCategoria(indice).map((video) => <Card idVideo={video.id} key={video.id} />)}
             </Category>
           )
-        }
+        } */}
 
       </Container>
       <Footer />
